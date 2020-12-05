6838a630a70dc7bc6e23ffc5c12d3fc938852a7d
Hayden Young <haydenyounglex@ohtmail.com>
2020-12-04 20:21:41 -0500

    init

---
+++ schemas/category.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'category',
+  title: 'Category',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Category Name',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+  ],
+}
